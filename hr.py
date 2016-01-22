9c3b3cc04a9bb5dd897fc07475e8b5d5124b2b9a
Brandon Milton <brandon.milton21@gmail.com>
2016-01-22 15:29:44 -0800

    Removed old example and renamed new one

--- hr.py
+++ hr.py
@@ -1,18 +1,5 @@
-"""
-hr.py
+from scraper import Scraper
 
-A sample script to show how to use scraper.py
-"""
-from scraper import Competitor, Scraper
-
-competitorDict = {} # Maps competitors username to problems solved
-leaderboardScraper = Scraper()
-while True:
-    for competitor in Competitor.get_competitor_list(leaderboardScraper):
-        if competitor.username not in competitorDict:
-            competitorDict[competitor.username] = competitor.completedCount
-            print("COMPETITOR {} NOW BEING TRACKED".format(competitor.username))
-        elif not competitor.completedCount == competitorDict[competitor.username]:
-            print("COMPETITOR {} HAS COMPLETED {} ADDITIONAL PROBLEMS. TOTAL={}".format(
-                competitor.username, competitor.completedCount - competitorDict[competitor.username],
-                competitor.completedCount))
+scr = Scraper()
+for competitorList in scr.scrape():
+    print(competitorList)
--- hr2.py
+++
@@ -1,5 +0,0 @@
-from scraper import Scraper
-
-scr = Scraper()
-for competitorList in scr.scrape():
-    print(competitorList)
